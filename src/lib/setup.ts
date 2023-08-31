f9a8a2bc27a5d1a651d35aa812b2d3906783d314
Nicolai Blak Walther <niwal19@student.sdu.dk>
2023-08-31 12:50:48 +0200

    Moved env-files

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,17 +1,22 @@
-import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
-import '@sapphire/plugin-api/register';
-import '@sapphire/plugin-hmr/register';
-import '@sapphire/plugin-logger/register';
-import { setup, type ArrayString } from '@skyra/env-utilities';
-import { join } from 'path';
+import {
+	ApplicationCommandRegistries,
+	RegisterBehavior,
+} from "@sapphire/framework";
+import "@sapphire/plugin-api/register";
+import "@sapphire/plugin-hmr/register";
+import "@sapphire/plugin-logger/register";
+import { setup, type ArrayString } from "@skyra/env-utilities";
+import { join } from "path";
 
-ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
+ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
+	RegisterBehavior.BulkOverwrite
+);
 
-const rootDir = join(__dirname, '..', '..');
-const srcDir = join(rootDir, 'src');
-setup({ path: join(srcDir, '.env') });
+const rootDir = join(__dirname, "..", "..");
+const srcDir = join(rootDir, "src");
+setup({ path: join(rootDir, ".env") });
 
-declare module '@skyra/env-utilities' {
+declare module "@skyra/env-utilities" {
 	interface Env {
 		OWNERS: ArrayString;
 	}
